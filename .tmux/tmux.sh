40a9c563507c7d9b36642fa1b46a3095ace752a9
James Gosnell <jamesgosnell@gmail.com>
2013-08-28 03:33:46 -0500

    ADDED -u parameter to force tmux to utf-8

--- .tmux/tmux.sh
+++ .tmux/tmux.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-alias tmux="TERM=screen-256color-bce tmux -2"
+
 if [ $# -eq 0 ]; then
-	echo attaching to most recent session
-	tmux a
+  echo attaching to most recent session
+  tmux -u a
 else
-	echo attaching to or creating: $1
-	tmux a -t $1 \
-		|| ~/.tmux/setup_$1.sh \
-		|| tmux new-session -s $1
+  echo attaching to or creating: $1
+  tmux -u a -t $1 \
+    || ~/.tmux/setup_$1.sh \
+    || tmux new-session -s $1
 fi
